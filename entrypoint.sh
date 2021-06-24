2799635f6d94aec9a98f0da1ca6c96bbbad2bb40
Lockwood <lockwooddev@gmail.com>
2021-06-24 11:47:46 +0300

    add output-file to action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 context='{"repository":"$1","ref":"$2","actor":"$3","run_id":"$4","sha":"$5"}'
-python /main.py --context $context --tool $6 --input-file $7 --renderer $8
+python /main.py --context $context --tool $6 --input-file $7 --output-file $8 --renderer $9
